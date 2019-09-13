3fc471037a3fe56771ebc4ffcaa9ef52ba3d472d
Andy Hsieh <andy.hsieh@lexer.com.au>
2019-09-14 05:09:07 +1000

    Add test project and workflows

---
+++ go1.11/linter/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -exuo pipefail
+
+echo $INPUT_TEST
+echo $TEST
+echo "INSIDE $INSIDE"
+echo "OUTSIDE $INSIDE"
+
+test=${INPUT_TEST:-off}
+
+if [[ $test == "off" ]]; then
+  echo "hello there"
+fi
+
