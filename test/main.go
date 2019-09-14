060c89996c4551765f1a7dd6b9dd7093e497dfdf
Andy Hsieh <andy.hsieh@lexer.com.au>
2019-09-15 05:12:45 +1000

    Fix based on feedback from Staticcheck

--- test/main.go
+++ test/main.go
@@ -2,24 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"test-project/test"
 )
 
 func main() {
-	fmt.Println("vim-go")
-	test.Add(1, 2)
-	_, err := strconv.ParseBool("true")
-	if err != nil {
-		fmt.Println("error found")
-	}
-	_, err = strconv.ParseBool("false")
-
-	err = TestFunc()
-}
-
-// Hllo world
-func TestFunc() error {
-	fmt.Println("Hello Wrld")
-	return nil
+	result := test.Add(1, 2)
+	fmt.Printf("result: %+v", result)
 }
