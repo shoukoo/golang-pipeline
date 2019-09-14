44bd746ff0f6d62f9326be84b041044fc8fce14d
Andy Hsieh <andy.hsieh@lexer.com.au>
2019-09-15 08:00:32 +1000

    Stop using symlink because Docker doesn't like it

--- go1.11/linter/setup.sh
+++ go1.11/linter/setup.sh
@@ -1,1 +1,11 @@
-../../setup.sh
+#!/bin/bash
+set -exuo pipefail
+
+export GO111MODULE=on
+
+# Set PROJECT_PATH to change your working directory
+if [ -z "${PROJECT_PATH}" ]; then
+  PROJECT_PATH="."
+fi
+
+cd $PROJECT_PATH
