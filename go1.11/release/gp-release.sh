efbdc156f5a5532815f3c38ecdd5fea32a813459
Andy Hsieh <andy.hsieh@lexer.com.au>
2019-10-03 05:22:58 +1000

    Make sync to other version of release

--- go1.11/release/gp-release.sh
+++ go1.11/release/gp-release.sh
@@ -11,19 +11,25 @@ NAME="${PROJECT_NAME}-${GOOS}-${GOARCH}"
 echo "Building $NAME under $GOOS/$GOARCH"
 go build -o "$NAME"
 
+if [ $GOOS == 'windows' ]; then
+    EXT='.exe'
+fi
+
+tar cvfz tmp.tgz "${NAME}${EXT}"
+
 curl \
   --fail \
   -X POST \
   --data-binary @${NAME}\
   -H 'Content-Type: application/octet-stream' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "${UPLOAD_URL}?name=${NAME}"
+  "${UPLOAD_URL}?name=${NAME}.tar.gz"
 
-CHECKSUM=$(md5sum "$NAME" | cut -d ' ' -f 1)
+CHECKSUM=$(sha256sum "$NAME" | cut -d ' ' -f 1)
 
 curl \
   -X POST \
   --data "$CHECKSUM" \
   -H 'Content-Type: text/plain' \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-  "${UPLOAD_URL}?name=${NAME}_checksum.txt"
+  "${UPLOAD_URL}?name=${NAME}_checksum_sha256.txt"
